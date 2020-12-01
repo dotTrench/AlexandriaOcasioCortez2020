e1c404e2a66928620cd8df147e8071defc004d6d
Erik Strandgård <erik.strandgard@gmail.com>
2020-12-01 19:15:14 +0100

    answer tests

--- src/day1.rs
+++ src/day1.rs
@@ -25,6 +25,7 @@ pub fn t2(input: Vec<u32>) -> Option<u32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::util::*;
 
     #[test]
     fn test_t1() {
@@ -32,7 +33,17 @@ mod tests {
     }
 
     #[test]
+    fn test_t1_answer() {
+        assert_eq!(Some(157059), t1(read_input_vec("inputs/day1.txt")))
+    }
+
+    #[test]
     fn test_t2() {
         assert_eq!(Some(241861950), t2(vec![1721, 979, 366, 299, 675, 1456]))
+    }
+
+    #[test]
+    fn test_t2_answer() {
+        assert_eq!(Some(165080960), t2(read_input_vec("inputs/day1.txt")))
     }
 }
